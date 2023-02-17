5923e75ede7fc498a362a1c5054559f08f3d61f3
Raghav <kumarraghav71950@gmail.com>
2023-02-17 21:54:08 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
 
 import React, { useState } from 'react'
 import '../styles/App.css';
-const App = () => {
-  
-  )
+function App() {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(50);
+  const [t, setT] = useState(50);
+  return (
+    <div id="main">
+      <div id="max-sum-holder">
+        Input max sum:-
+        <input
+          type={"number"}
+          value={t}
+          onChange={(e) => setT(e.target.value)}
+        />
+      </div>
+      <div id="range-a-holder">
+        <input
+          type={"range"}
+          value={a}
+          onChange={(e) => setA(e.target.value)}
+          max={t - b}
+        />
+        <span id="range-a-value">{a}</span>
+      </div>
+      <div id="range-b-holder">
+        <input
+          type={"range"}
+          value={b}
+          onChange={(e) => setB(e.target.value)}
+          max={t - a}
+        />
+        <span id="range-b-value">{b}</span>
+      </div>
+      <div id="sum">
+        {[a, b].map(Number).reduce((sum, curr) => sum + curr, 0)}
+      </div>
+    </div>
+  );
 }
 
 
